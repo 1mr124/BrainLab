f6c34bd9e1ca969f811bb5ded5cb6813f24c7a20
Mr12 <221bst221@gmail.com>
2024-08-10 00:41:57 +0300

    First Push

---
+++ static/timeLine.js
@@ -0,0 +1,7 @@
+document.getElementById('fastRead').addEventListener('click', function() {
+    window.location.href = 'fastRead.html'; // Replace with your URL
+});
+
+document.getElementById('mindGames').addEventListener('click', function() {
+    window.location.href = 'mindGames.html'; // Replace with your URL
+});
